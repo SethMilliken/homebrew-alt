7ec7fbcd9b51011032dce0d3df1ad73bcc651a4a
Seth Milliken <seth.milliken@urbanairship.com>
2016-09-21 11:36:19 -0700

    switch from sha1 to sha256
    `curl -O http://synergy.googlecode.com/files/synergy-1.3.6p2-MacOSX-Universal.zip`
    `shasum -a 256 synergy-1.3.6p2-MacOSX-Universal.zip`

--- synergy.rb
+++ synergy.rb
@@ -5,12 +5,12 @@ class Synergy < Formula
   # http://code.google.com/p/synergy-plus/issues/detail?id=47
   # Do not bump this to 1.3.7 or newer until that issue is resolved.
   url 'http://synergy.googlecode.com/files/synergy-1.3.6p2-MacOSX-Universal.zip'
-  sha1 '5b9336fb553db17bd109e3f2bca517af18ed3863'
+  sha256 '8863527a9c7bf101f8ff9fa1b0d8d94096c27aacf04e3fa3107bb8ab92207e40'
   homepage 'http://synergy-foss.org'
 
   devel do
     url 'http://synergy.googlecode.com/files/synergy-1.4.3-MacOSX106-Universal.zip'
-    sha1 'aa60fa6ac975dd22dc095d6cd9f3dc755b91bb64'
+    sha256 '7e748bd7af692afc37ad02788186e4964beaffd9b6f61deb21da6a5987bd1d61'
   end
 
   def install
